8e593cd1521a8ec0822513b90a0327e02005cad5
Marco Bellezza <bellezza.m@capp.it>
2021-11-03 09:33:30 +0100

    Commentato per bene

--- Program.cs
+++ Program.cs
@@ -2,26 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 
+var diskCount = 3;
+
+// setup state
+
+// state
 var towers = new IEnumerable<int>[]
 {
-    Enumerable.Range(1, 3),
+    Enumerable.Range(1, diskCount),
     Enumerable.Empty<int>(),
     Enumerable.Empty<int>()
 };
 
 var moveCount = 0;
 
+// start game
+
 while (true)
 {
-    foreach (var tower in towers)
+    // output state
+    for (int i = 0; i < towers.Count(); i++)
     {
-        Console.WriteLine("| " + string.Join(' ', tower));
+        var tower = towers[i];
+        Console.WriteLine(i + " | " + string.Join(' ', tower));
     }
+    Console.WriteLine("------");
 
+    // on input
     var input = Console.ReadLine()!;
+    Console.WriteLine("------");
 
     int from, to;
 
+    // validate input as (from, to)
     try
     {
         var indexes = input.Split(" ");
@@ -30,36 +43,44 @@ while (true)
     }
     catch
     {
+        // output error
         Console.WriteLine("Describe your next move with a '<from> <to>' notation, where <from> and <to> are the zero-based index of the towers.");
         continue;
     }
 
+    // validate (from, to) as indexes
     if ((from, to) is (< 0 or > 2, _) or (_, < 0 or > 2))
     {
+        // output error
         Console.WriteLine("Indexes must be between 0 and 2.");
         continue;
     }
 
-    if (towers[from].Count() == 0)
+    // validate from-tower as filled
+    if (!towers[from].Any())
     {
+        // output error
         Console.WriteLine("You cannot move a disk from an empty tower.");
         continue;
     }
 
+    // validate to-tower as filled and top disk of to-tower smaller than top disk of from-tower as move
     if (towers[to].Any() && towers[to].Last() > towers[from].Last())
     {
+        // output error
         Console.WriteLine("You cannot move a disk on a smaller disk.");
         continue;
     }
 
+    // apply move to towers
     towers[to] = towers[to].Append(towers[from].Last());
     towers[from] = towers[from].SkipLast(1);
     moveCount++;
 
-    Console.WriteLine(input);
-
-    if (towers[0].Count() is 0 && towers[1].Count() is 0 && towers[2].Count() is 3)
+    // check game solved from towers
+    if (!towers[0].Any() && !towers[1].Any() && towers[2].Count() == diskCount)
     {
+        // output game solved with state
         Console.WriteLine("You won the game in {0} moves!", moveCount);
         break;
     }
