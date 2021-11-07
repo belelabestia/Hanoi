454f3fc5cfe7e2df74b2ef3604fbfbdd7c6a750b
Marco Bellezza <bellezza.m@capp.it>
2021-11-07 18:12:26 +0100

    Spostato competenze di Input dentro IO

--- Program.cs
+++ Program.cs
@@ -1,117 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 
-new Game(3).Run();
+IO.Run(new Game(3));
 
-class Game
+static class IO
 {
-    private int diskCount;
-    private Tower[] towers;
-    private int moveCount;
-
-    public Game(int diskCount)
-    {
-        this.diskCount = diskCount;
-        this.moveCount = 0;
-        towers = InitTowers(diskCount);
-    }
-
-    public bool GameSolved =>
-        towers[0].IsEmpty &&
-        towers[1].IsEmpty &&
-        towers[2].Height == diskCount;
-
-    public void Run()
+    public static void Run(Game game)
     {
         while (true)
         {
-            IO.OutputState(towers);
+            IO.OutputState(game.Towers);
 
             var input = IO.AwaitInput();
 
-            try { HandleInput(input); }
+            try { HandleInput(game, input); }
             catch { continue; }
 
-            if (GameSolved)
+            if (game.GameSolved)
             {
-                IO.OutputGameSolved(moveCount);
+                IO.OutputGameSolved(game.MoveCount);
                 break;
             }
         }
     }
 
-    private void HandleInput(string input)
+    private static void HandleInput(Game game, string input)
     {
         try
         {
             var move = IO.ReadMove(input);
-            towers = ApplyMove(move);
-            moveCount++;
+            game.ApplyMove(move);
         }
-        catch (InputMoveException) { IO.OutputInputError(); throw; }
         catch (IndexException) { IO.OutputIndexError(); throw; }
+        catch (InputMoveException) { IO.OutputInputError(); throw; }
         catch (EmptyTowerException) { IO.OutputTowerError(); throw; }
         catch (InvalidMoveException) { IO.OutputMoveError(); throw; }
     }
 
-    private Tower[] ApplyMove(Move move)
-    {
-        towers[move.To.Value] = towers[move.To.Value].Put(towers[move.From.Value].Top);
-        towers[move.From.Value] = towers[move.From.Value].Take();
-
-        return towers;
-    }
-
-    private Tower[] InitTowers(int diskCount) =>
-        new IEnumerable<Disk>[]
-        {
-            Enumerable.Range(1, diskCount)
-                .Select(Disk.FromSize)
-                .Reverse(),
-            Enumerable.Empty<Disk>(),
-            Enumerable.Empty<Disk>()
-        }
-            .Select(Tower.FromDisks)
-            .ToArray();
-}
-
-static class IO
-{
-    public static string AwaitInput()
+    private static string AwaitInput()
     {
         var input = Console.ReadLine()!;
         Console.WriteLine("------");
         return input;
     }
 
-    public static void OutputInputError() =>
-        Console.WriteLine("Describe your next move with a '<from> <to>' notation, where <from> and <to> are the zero-based index of the towers.");
-
-    public static void OutputIndexError() =>
-        Console.WriteLine("Indexes must be between 0 and 2.");
-
-    public static void OutputTowerError() =>
-        Console.WriteLine("You cannot move a disk from an empty tower.");
-
-    public static void OutputMoveError() =>
-        Console.WriteLine("You cannot move a disk on a smaller disk.");
-
-    public static void OutputGameSolved(int moveCount) =>
-        Console.WriteLine("You won the game in {0} moves!", moveCount);
-
-    public static void OutputState(Tower[] towers)
-    {
-        for (int i = 0; i < towers.Count(); i++)
-        {
-            var tower = towers[i];
-            Console.WriteLine(i + " | " + string.Join(' ', tower.Disks.Select(disk => disk.Size.ToString())));
-        }
-
-        Console.WriteLine("------");
-    }
-
-    public static Move ReadMove(string input)
+    private static Move ReadMove(string input)
     {
         try
         {
@@ -123,11 +58,75 @@ static class IO
 
             return new Move(indexes[0], indexes[1]);
         }
-        catch
-        {
-            throw new InputMoveException();
-        }
+        catch (IndexException) { throw; }
+        catch { throw new InputMoveException(); }
     }
+
+    private static void OutputState(IEnumerable<Tower> towers)
+    {
+        for (int i = 0; i < towers.Count(); i++)
+        {
+            var tower = towers.ElementAt(i);
+            Console.WriteLine(i + " | " + string.Join(' ', tower.Disks.Select(disk => disk.Size.ToString())));
+        }
+
+        Console.WriteLine("------");
+    }
+
+    private static void OutputInputError() =>
+        Console.WriteLine("Describe your next move with a '<from> <to>' notation, where <from> and <to> are the zero-based index of the towers.");
+
+    private static void OutputIndexError() =>
+        Console.WriteLine("Indexes must be between 0 and 2.");
+
+    private static void OutputTowerError() =>
+        Console.WriteLine("You cannot move a disk from an empty tower.");
+
+    private static void OutputMoveError() =>
+        Console.WriteLine("You cannot place a disk on a smaller disk.");
+
+    private static void OutputGameSolved(int moveCount) =>
+        Console.WriteLine("You won the game in {0} moves!", moveCount);
+}
+
+class Game
+{
+    public int DiskCount { get; private set; }
+    public ImmutableArray<Tower> Towers { get; private set; }
+    public int MoveCount { get; private set; }
+
+    public Game(int diskCount)
+    {
+        this.DiskCount = diskCount;
+        this.MoveCount = 0;
+        Towers = InitTowers(diskCount);
+    }
+
+    public bool GameSolved =>
+        Towers.ElementAt(0).IsEmpty &&
+        Towers.ElementAt(1).IsEmpty &&
+        Towers.ElementAt(2).Height == DiskCount;
+
+    public void ApplyMove(Move move)
+    {
+        Towers = Towers
+            .SetItem(move.To.Value, Towers[move.To.Value].Put(Towers[move.From.Value].Top))
+            .SetItem(move.From.Value, Towers[move.From.Value].Take());
+
+        MoveCount++;
+    }
+
+    private ImmutableArray<Tower> InitTowers(int diskCount) =>
+        new IEnumerable<Disk>[]
+        {
+            Enumerable.Range(1, diskCount)
+                .Select(Disk.FromSize)
+                .Reverse(),
+            Enumerable.Empty<Disk>(),
+            Enumerable.Empty<Disk>()
+        }
+            .Select(Tower.FromDisks)
+            .ToImmutableArray();
 }
 
 record Tower(IEnumerable<Disk> Disks)
