4d1150043930c432acc0843d8a801a24e888cd60
Marco Bellezza <bellezza.m@capp.it>
2021-11-25 22:29:42 +0100

    Migliorato errori su args

--- Conf.cs
+++ Conf.cs
@@ -2,16 +2,27 @@ using System;
 
 record Conf(Mode Mode, int Count)
 {
-    public static Conf FromArgs(string[] args) =>
-        new Conf(
-            Mode: args[0].ToLower() switch
-            {
-                "play" => Mode.Play,
-                "solve" => Mode.Solve,
-                _ => throw new ArgumentException()
-            },
-            Count: int.Parse(args[1])
-        );
+    public static Conf FromArgs(string[] args)
+    {
+        try
+        {
+            return new Conf(
+                Mode: args[0].ToLower() switch
+                {
+                    "play" => Mode.Play,
+                    "solve" => Mode.Solve,
+                    var a => throw new ArgumentException($"Invalid argument: '{a}'.")
+                },
+                Count: int.Parse(args[1])
+            );
+        }
+        catch (Exception e) { throw new ConfException(e); }
+    }
 }
 
 enum Mode { Play, Solve }
+
+class ConfException : ArgumentException
+{
+    public ConfException(Exception e) : base($"Invalid hanoi parameters: {e.Message}", e) { }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
-﻿Hanoi.Run(Conf.FromArgs(args));
+﻿using System;
+
+try { Hanoi.Run(Conf.FromArgs(args)); }
+catch (ConfException e) { Console.WriteLine(e.Message); }
