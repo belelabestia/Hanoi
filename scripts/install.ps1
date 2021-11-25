2b68a073eb2a43e0474b8e394ab62bd0e58014d6
Marco Bellezza <bellezza.m@capp.it>
2021-11-25 22:28:52 +0100

    Sistemati script

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,5 +1,5 @@
 echo '$_: dotnet pack'
 dotnet pack
 
-echo '$_: dotnet tool install --global --add-source nuget Hanoi'
-dotnet tool install --global --add-source nuget Hanoi
+echo '$_: dotnet tool install --global --add-source nupkg Hanoi'
+dotnet tool install --global --add-source nupkg Hanoi
