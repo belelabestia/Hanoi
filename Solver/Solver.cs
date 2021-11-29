d35c5e2ef4d05f691be3d1da7fde45c76c507441
Marco Bellezza <bellezza.m@capp.it>
2021-11-29 22:17:39 +0100

    Sistemata concorrenza nella stampa dei risultati

--- Solver/Solver.cs
+++ Solver/Solver.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 
 static class Solver
 {
+    private static bool printing = false;
+
     public static void OutputSolved(int moveCount, IEnumerable<Move> moves)
     {
+        while (printing);
+
+        printing = true;
+
         Console.WriteLine("Solved in {0} moves!", moveCount);
 
         foreach (var move in moves)
-        {
             Console.WriteLine("{0} -> {1}", move.From.Value, move.To.Value);
-        }
+
+        printing = false;
     }
 
     public static void OutputFailed() =>
